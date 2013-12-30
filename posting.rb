40004f6a3601235097798f6d58763dfa5fa8e02d
sasamijp <k.seiya28@gmail.com>
2013-12-31 00:59:45 +0900

    とりあえずコミットしとけばなんとかなるやろ

--- posting.rb
+++ posting.rb
@@ -17,80 +17,108 @@ TweetStream.configure do |config|
    config.auth_method            = :oauth
 end
 
-puts "select a dictionary from the list below"
-
-Dir::foreach('.') do |f|
-  if File::ftype(f) == "file" && f.include?('.txt') then
-    p f
-  end
-end
-
 client = TweetStream::Client.new
-s = File.read("#{gets.chop}", :encoding => Encoding::UTF_8)
+s = File.read("dictionary.txt", :encoding => Encoding::UTF_8)
+mode = "japanese"
+Twitter.update_profile(:name => "ささみbot(日本語モード)")
+words = s.split("\n")
+puts "ready to streaming"
 
 client.userstream do |status|
-  isreply = status.text.include?("@sa2mi")
-  isrt = status.text.include?("RT")
-  if isreply && !isrt then
+  
+  if status.text.include?("@sa2mi") && !status.text.include?("RT") then
+    puts status.text
+    if status.text[0..10] == "@sa2mi mode"
+      case status.text.sub("@sa2mi mode ","")
+      when "日本語" then
+        s = File.read("dictionary.txt", :encoding => Encoding::UTF_8)
+        mode = "japanese"
+        Twitter.update_profile(:name => "ささみbot(日本語モード)")
+      when "大日本帝国憲法" then
+        s = File.read("dictionary_kenpou.txt", :encoding => Encoding::UTF_8)
+        mode = "kenpou"
+        Twitter.update_profile(:name => "ささみbot(帝国憲法モード)")
+      when "奥の細道" then
+        s = File.read("dictionary_matsuo.txt", :encoding => Encoding::UTF_8)
+        mode = "hosomichi"
+        Twitter.update_profile(:name => "ささみbot(奥のほそ道モード)")
+      when "日本国憲法" then
+        s = File.read("kenpoujapan.txt", :encoding => Encoding::UTF_8)
+        mode = "kenpou_new"
+        Twitter.update_profile(:name => "ささみbot(日本国憲法モード)")
+      when "ホモ" then
+        s = File.read("inmu.txt", :encoding => Encoding::UTF_8)
+        mode = "homo"
+        Twitter.update_profile(:name => "ささみbot(ホモモード)")
+      when "歌う野獣"
+        s = File.read("sing.txt", :encoding => Encoding::UTF_8)
+        mode = "yaju"
+        Twitter.update_profile(:name => "ささみbot(歌う野獣モード)")
+      end
+    end
 
-    isStatement = true
-    words = s.split("\n")
     begins=[]
     text = ''
+    isStatement = true
 
-    words.each do |word|
+    words.each do |word| #最初の一文節の候補を選ぶ
       sword = word.split
-      if sword[0] == "beginpoint" then
-   	    begins.push(word)
-      end
-      if !sword[sword.length-1] == "endpoint" then
-        isStatement = false
-      else
-        isStatement = true
-      end
+      begins.push(word) if sword[0] == "beginpoint"
+      isStatement = false if sword[sword.length-1] == "endpoint" #beginpointからすぐendpointに移行する場合、単語ツイートとして処理する
     end
 
     rnum = rand(begins.length)
-    first = begins[rnum].sub('beginpoint ','')
-    text << first
+    first = begins[rnum].sub('beginpoint ','') #最初の一文節を候補の中から乱数で決定
+    text << first #なんかこう書くと速いらしい
 
+    
+
+    puts isStatement
     if isStatement then
-      candidate=[]
+      candidate = []
       10.times do
         words.each do |word|
-        stext=text.split
-        endword = stext[stext.length-1]
-        if word.split[0] == endword then
-          candidate.push(word.sub(endword,''))
+          stext=text.split
+          endword = stext[stext.length-1]
+          candidate.push(word.sub(endword,'')) if word.split[0] == endword 
         end
       end
 
-      while true do
+      while addword != stext[stext.length-1] do
         rnum = rand(candidate.length)
         addword = candidate[rnum]
         stext = text.split
-        if addword == stext[stext.length-1] then
-          addword = candidate[rnum]
-        else
-          break
-        end
+        addword = candidate[rnum] if addword == stext[stext.length-1]
       end
 
       text << candidate[rnum]
-      stext = text.split
-      p stext[stext.length-1]
-      if stext[stext.length-1] == "endpoint" then
-        break
-      end
+      p text.split[stext.length-1]
+      break if text.split[stext.length-1] == "endpoint"
     end
 
-    text = text.gsub(' endpoint','')
-    text = text.gsub(' ','')
-    text = text.gsub('@','')
+    text.gsub!(/ endpoint| |@/, " endpoint" => "", " " => "", "@" => "")
+
+    if mode == "japanese" then
+      emotions = ''
+      case status.text[-1]
+        when "！","!"
+          emotions = "！"
+        when "ｗ","w"
+          emotions = "ｗ"
+        when "な","ね"
+          emotions = "。"
+        when "。","…","."
+          emotions = "..."
+        when "？","?"
+          text.gsub!("？","")
+      end
+      text << emotions
+    end
+
     puts text
     text = "@#{status.user.screen_name} #{text}"
     option = {"in_reply_to_status_id"=>status.id.to_s}
     Twitter.update text,option
-  end             #このend絶対いらないと思うんだけどないとエラー出る死ね
+
   end
 end
--- writing.rb
+++ writing.rb
@@ -16,22 +16,21 @@ inputs = []
 while true do
   if n == 0 then
     inputs[counter] = "beginpoint #{text[n]} #{text[n+1]} #{text[n+2]} #{text[n+3]}"
-    n+=3
+    n+=2
+  elsif n+5>=text.length+1 then
+    break
   else
     inputs[counter] = "#{text[n]} #{text[n+1]} #{text[n+2]} #{text[n+3]} #{text[n+4]}"
-    n+=4
+    n+=1
   end
   counter+=1
-  if n+1>=text.length+1 then
-    break
-  end
 end
 p inputs
 
 count = 0
 
 inputs.each do|input|
-  File.open("dictionary.txt","a") do |file|
+  File.open("new_dictionary.txt","a") do |file|
     file.write("#{input}\n")
   end
   count+=1
