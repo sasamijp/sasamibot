aa01ba3388b58a2e0c73bae2974e1a85913b44cd
sasamijp <k.seiya28@gmail.com>
2014-01-09 17:21:01 +0900

    神の所業

--- Gemfile
+++ Gemfile
@@ -5,3 +5,5 @@ gem 'twitter',"4.8.1"
 gem 'tweetstream',"2.5.0"
 
 gem 'bimyou_segmenter',"1.2.0"
+
+gem 'natto'
--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 # -*- encoding: utf-8 -*-
 require 'tweetstream'
 require 'twitter'
+require 'bimyou_segmenter'
+require 'natto'
 require './key.rb'
 
 Twitter.configure do |config|
@@ -16,25 +18,76 @@ TweetStream.configure do |config|
    config.oauth_token_secret  = Const::ACCESS_TOKEN_SECRET
    config.auth_method            = :oauth
 end
-
 client = TweetStream::Client.new
-s = File.read("new_dictionary.txt", :encoding => Encoding::UTF_8)
-words = s.split("\n")
-Twitter.update_profile(:name => "ささみbot(日本語モード)")
-puts "ready to streaming"
 
 client.userstream do |status|
-  
-  if status.text.include?("@sa2mi") && !status.text.include?("RT") then
-    puts status.text
+if status.text.include?("@sa2mi") && !status.text.include?("RT") then
+nouns=[]
+input = status.text.sub("@sa2mi ","")
+nm = Natto::MeCab.new
+nm.parse(input) do |n|
+  type = n.feature.split(",")[0]
+  if type == "名詞"
+    nouns.push(n.surface)
+  end
+end
+c = []
+Twitter.search("#{nouns[0]}", :count => 50, :result_type => "latest").results.map do |status|
+  next if status.text.include?("RT") or status.text.include?("http") or status.text.include?(".co") or status.text.include?("@")
+  c.push("#{status.text.gsub("\n","")}")
+end
+print c
+dictionary=[]
+c.each do |cc|
+  text=BimyouSegmenter.segment(cc)
+  p text
+  p text.length
+  text.push("endpoint")
+  p text
+  p text.length
+    if text.length == 5 
+      deathflag = true
+    end
+  if !deathflag
+    n=0
+    counter = 0
+    inputs = []
+
+    while true do
+      breakflag = false
+      if n == 0 then
+        inputs[counter] = "beginpoint #{text[n]} #{text[n+1]} #{text[n+2]} #{text[n+3]}"
+        n+=2
+      elsif n+2>=text.length+1 then
+        breakflag = true
+      else
+        inputs[counter] = "#{text[n]} #{text[n+1]} #{text[n+2]} #{text[n+3]} #{text[n+4]}"
+        n+=1
+      end
+      break if breakflag
+      counter+=1
+    end
+    p inputs
+    inc = inputs.include?("endpoint")
+
+    inputs.each do|input|
+      if !inc
+        dictionary.push("#{input}")
+      else
+        dictionary.push("#{input} endpoint")
+      end
+    end
+  end
+end
+
 
     begins=[]
     text = ''
     isStatement = true
 
-    words.each do |word| #最初の一文節の候補を選ぶ
+    dictionary.each do |word| #最初の一文節の候補を選ぶ
       sword = word.split
-      begins.push(word) if sword[0] == "beginpoint"
+      begins.push(word) if sword[0] == "beginpoint" 
     end
 
     rnum = rand(begins.length)
@@ -49,8 +102,8 @@ p text
     if isStatement
       puts isStatement
       candidate = []
-      10.times do
-        words.each do |word|
+      7.times do
+        dictionary.each do |word|
           stext=text.split
           endword = stext[stext.length-1]
           candidate.push(word.sub(endword,'')) if word.split[0] == endword
@@ -73,24 +126,7 @@ p text
     text.gsub!(" endpoint","")
     text.gsub!(" ","")
     text.gsub!("@","")
-p "4"
 
-    emotions = ''
-    case status.text[-1]
-      when "！","!"
-        emotions << "！"
-      when "ｗ","w"
-        emotions << "ｗ"
-      when "な","ね"
-        emotions << "。"
-      when "。","…","."
-        emotions << "..."
-      when "？","?"
-        text.gsub!("？","")
-    end
-    text << emotions
-
-p "5"
     puts text
     text = "@#{status.user.screen_name} #{text}"
     option = {"in_reply_to_status_id"=>status.id.to_s}
